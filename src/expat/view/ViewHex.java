50289829d0dba37f0c337d6f20f538bae59d6f47
Sibesiech <rino.vanoni@gmail.com>
2017-03-29 09:37:47 +0200

    Basic HexView refined2

--- src/expat/view/ViewHex.java
+++ src/expat/view/ViewHex.java
@@ -1,6 +1,8 @@
 package expat.view;
 
+import expat.model.board.ModelHex;
 import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Polygon;
 
@@ -12,18 +14,24 @@ public class ViewHex extends Polygon {
     double[] yPoints = new double[]{0, 0, 0.43879, 0.87758, 0.87758, 0.43879};
     double xOffset = 0.0;
     double yOffset = 0.0;
-    double size = 100;
+    double size;
     Image wool = new Image("expat/img/Wiese_Sheep.png"); //TODO; set better path, use String variable;
 
-    public ViewHex(double xOffset, double yOffset) {// TODO: Implement ModelHex as Parameter and CoordinateCalculator which calculates die actual position of the hex and returns xOffset and yOffset OR calculate offset for whole child.
+    /** Generates a polygon, to be added as a child on the BoardPane.
+     * @param size in pixel is the widht of the hex.
+     * @param xOffset is the horizontal offset in pixel from with 0 being on the left side of the screen.
+     * @param yOffset is the
+     */
+    public ViewHex(int size, double xOffset, double yOffset) {// TODO: Implement Hex as Parameter and CoordinateCalculator which calculates die actual position of the hex and returns xOffset and yOffset OR calculate offset for whole child.
+        this.size = size;
         this.xOffset = xOffset;
         this.yOffset = yOffset;
 
         for (int i = 0; i < 6; i++) {
-            this.getPoints().addAll(new Double[]{xPoints[i]*size,yPoints[i]*size});
+            this.getPoints().addAll(new Double[]{xPoints[i] * size, yPoints[i] * size});
         }
 
-        this.setFill(new ImagePattern(wool,0,0,1,1,true));
+        this.setFill(new ImagePattern(wool, 0, 0, 1, 1, true));
         this.setLayoutX(xOffset);
         this.setLayoutY(yOffset);
 
