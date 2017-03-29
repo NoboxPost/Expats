6847be3263dc2aa86d6c6c3b15886fecbfd11fc6
Sibesiech <rino.vanoni@gmail.com>
2017-03-29 08:25:09 +0200

    Basic HexView refined

--- src/expat/model/board/Water.java
+++ src/expat/model/board/Water.java
@@ -6,8 +6,8 @@ import expat.model.Material;
  * Created by vanonir on 22.03.2017.
  */
 public class Water extends Hex {
-    public Water(int id) {
-        super(id);
+    public Water(int xCoord,int yCoord) {
+        super(xCoord,yCoord);
         type = "Water";
         material = new Material(new int[]{0,0,0,0,0}); //TODO: add corresponding Material
     }
