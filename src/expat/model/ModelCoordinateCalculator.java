76ac9990158beb228043f966ab28c4e17d83fbfd
Sibesiech <rino.vanoni@gmail.com>
2017-04-06 14:51:27 +0200

    JavaDoc Added2

--- src/expat/model/ModelCoordinateCalculator.java
+++ src/expat/model/ModelCoordinateCalculator.java
@@ -3,7 +3,7 @@ package expat.model;
 import expat.model.board.ModelHex;
 
 /**
- * is responsible for
+ * is responsible for callculation from hex coordinates to building coordinates.
  * <p>
  * created on 31.03.2017
  *
@@ -11,17 +11,20 @@ import expat.model.board.ModelHex;
  */
 public class ModelCoordinateCalculator {
 
-    private int xHexSize, yHexSize, xBuildingSize, yBuildingSize;
 
-    public ModelCoordinateCalculator(int xHexSize, int yHexSize) {
-        this.xHexSize = xHexSize;
-        this.yHexSize = yHexSize;
-        xBuildingSize = (xHexSize * 6) + 2;
-        yBuildingSize = yHexSize * 4;
-    }
-
-
-
+    /**
+     * Calculates the relative 0;0 Coordinate of a Hex in the Building Coordinate System, according to given Hex.
+     *
+     *  012345678
+     * 0oo-----oo
+     * 1o/ooooo\o
+     * 2{ooooooo}---
+     * 3o\ooooo/o
+     * 4oo----{oo
+     *
+     * @param hex Hex which contains the coordinates.
+     * @return an int[] Array int[0] corresponds to X Coordinate of Building Grid, int[1] corresponds to Y coordinate of the Building Grid.
+     */
     public int[] hexCoordToBuildingCoord(ModelHex hex){
          int[] coords = hex.getCoords();
         return hexCoordToBuildingCoord(coords[0],coords[1]);
